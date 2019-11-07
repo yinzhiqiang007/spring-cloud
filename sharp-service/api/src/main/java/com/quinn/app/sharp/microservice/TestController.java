d1723e93c3c4361b17d1b4a17a2d72dd314fcc84
yin.zhiqiang <a2051896>
2019-11-07 14:41:38 +0800

    kai fa

--- sharp-service/api/src/main/java/com/quinn/app/sharp/microservice/TestController.java
+++
@@ -1,7 +0,0 @@
-package com.quinn.app.sharp.microservice;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController("test")
-public class TestController {
-}
--- ys-sharp/api/src/main/java/com/quinn/app/sharp/controller/UserController.java
+++ ys-sharp/api/src/main/java/com/quinn/app/sharp/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.quinn.app.sharp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@RestController("test")
-public class TestController {
+@RestController
+public class UserController {
 
-    @RequestMapping("/t1")
+    @RequestMapping("test")
     public String test(){
-        return "dsfsd是的范德萨"+new Date();
+        return "sdf234水电费"+new Date();
     }
 }

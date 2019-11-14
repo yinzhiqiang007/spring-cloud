bca701e5e143091bfecbfcb90c65fd33197c2bdb
yin.zhiqiang <a2051896>
2019-11-14 18:44:35 +0800

    develop

--- auth/src/main/java/com/quinn/app/auth/AuthApplication.java
+++ auth/src/main/java/com/quinn/app/auth/AuthApplication.java
@@ -2,9 +2,11 @@ package com.quinn.app.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
 
 

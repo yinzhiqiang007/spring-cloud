89c8081ebbd9c116d550a2418ff3342b1d2eedc7
zhiqiang.yin <zhiqiang.yin@transsion.com>
2020-11-02 20:14:43 +0800

    spring boot  2.3.3.RELEASE

--- auth/src/main/java/com/quinn/app/auth/AuthApplication.java
+++ auth/src/main/java/com/quinn/app/auth/AuthApplication.java
@@ -2,11 +2,9 @@ package com.quinn.app.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AuthApplication {
 
 
--- gateway/src/main/java/com/quinn/app/gateway/config/GatewayRoutes.java
+++ gateway/src/main/java/com/quinn/app/gateway/config/GatewayRoutes.java
@@ -1,24 +1,24 @@
-package com.quinn.app.gateway.config;
-
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class GatewayRoutes {
-
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r ->
-                        r.path("/quinn/**")
-                                .filters(
-                                        f -> f.stripPrefix(1)
-                                ).uri("lb://ys-sharp")
-                )
-                .build();
-    }
-}
+//package com.quinn.app.gateway.config;
+//
+//
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class GatewayRoutes {
+//
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r ->
+//                        r.path("/quinn/**")
+//                                .filters(
+//                                        f -> f.stripPrefix(1)
+//                                ).uri("lb://ys-sharp")
+//                )
+//                .build();
+//    }
+//}

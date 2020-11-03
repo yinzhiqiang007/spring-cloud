497b511dc63674fab7c628154951564d29161872
zhiqiang.yin <zhiqiang.yin@transsion.com>
2020-11-03 13:28:58 +0800

    spring boot  2.3.3.RELEASE

--- ys-sharp/api/src/main/java/com/quinn/app/sharp/SharpApplication.java
+++ ys-sharp/api/src/main/java/com/quinn/app/sharp/SharpApplication.java
@@ -17,10 +17,17 @@ import org.springframework.context.annotation.ComponentScan;
 public class SharpApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.cloud.nacos.config.namespace","082d82fe-e42f-44d4-ab3c-4dc888a784d3");
+        for(String s:args){
+            System.out.println("args:"+s);
+        }
+        String namespace = "082d82fe-e42f-44d4-ab3c-4dc888a784d3";
+
+        System.setProperty("spring.cloud.nacos.config.namespace",namespace);
+        System.setProperty("spring.cloud.nacos.discovery.namespace",namespace);
         ConfigurableApplicationContext applicationContext = SpringApplication.run(SharpApplication.class, args);
         String userName = applicationContext.getEnvironment().getProperty("ys.sharp.jdbc.test2");
         String userAge = applicationContext.getEnvironment().getProperty("ys.sharp.jdbc.test3");
+        System.out.println(applicationContext.getEnvironment().getProperty("spring.cloud.nacos.config.namespace"));
         System.err.println("user name :" + userName + "; age: " + userAge);
 //        SpringApplication.run(SharpApplication.class ,args);
 
